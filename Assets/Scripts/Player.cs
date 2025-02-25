52fb626855076b532e764c4449265d4dc3ead9b2
Alberto Lazari <alberto.lazari@gmail.com>
2025-02-25 19:54:54 +0100

    Character moves

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,97 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public enum MoveState
+    {
+        Idle,
+        Moving,
+    }
+
+    public enum Direction
+    {
+        Left,
+        Right,
+    }
+
+    [SerializeField]
+    private float m_Speed = 1f;
+
+    [SerializeField]
+    private Object m_Stage;
+
+    [SerializeField]
+    private LayerMask m_WallLayer;
+
+    [SerializeField]
+    private float m_CollisionDistance = 0.12f;
+
+    private MoveState m_MoveState = MoveState.Idle;
+    private Direction m_FacingDirection = Direction.Right;
+
+    private bool Facing(Direction i_Direction)
+    {
+        return m_FacingDirection == i_Direction;
+    }
+
+    private bool IsBlocked(Direction i_Direction)
+    {
+        Vector2 direction = i_Direction == Direction.Left ? Vector2.left : Vector2.right;
+        // Do not cast rays from player's feet, otherwise they will catch the floor
+        Vector2 rayOrigin = (Vector2)transform.position + Vector2.up * 0.1f;
+        return Physics2D.Raycast(rayOrigin, direction, m_CollisionDistance, m_WallLayer);
+    }
+
+    private void Update()
+    {
+        HandleInput();
+        if (CanMove())
+        {
+            Move();
+        }
+    }
+
+    private void HandleInput()
+    {
+        bool bLeft = Input.GetKey(KeyCode.LeftArrow);
+        bool bRight = Input.GetKey(KeyCode.RightArrow);
+        bool bLeftDown = Input.GetKeyDown(KeyCode.LeftArrow);
+        bool bRightDown = Input.GetKeyDown(KeyCode.RightArrow);
+
+        switch (m_MoveState)
+        {
+        case MoveState.Idle:
+            if (bLeft || bRight)
+            {
+                m_MoveState = MoveState.Moving;
+                m_FacingDirection = bRight ? Direction.Right : Direction.Left;
+            }
+            break;
+        case MoveState.Moving:
+            // Change direction
+            if (bLeftDown && Facing(Direction.Right) || bRightDown && Facing(Direction.Left))
+            {
+                m_FacingDirection = bRightDown ? Direction.Right : Direction.Left;
+            }
+            // Stop
+            if (!bLeft && Facing(Direction.Left) || !bRight && Facing(Direction.Right))
+            {
+                m_MoveState = MoveState.Idle;
+            }
+            break;
+        }
+    }
+
+    private bool CanMove()
+    {
+        bool bMoving = m_MoveState == MoveState.Moving;
+        bool bNotBlocked = !IsBlocked(m_FacingDirection);
+        return bMoving && bNotBlocked;
+    }
+
+    private void Move()
+    {
+        Vector2 direction = m_FacingDirection == Direction.Left ? Vector2.left : Vector2.right;
+        transform.position += (Vector3)direction * m_Speed * Time.deltaTime;
+    }
+}
