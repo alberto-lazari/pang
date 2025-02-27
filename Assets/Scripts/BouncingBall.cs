906e5a5e2dd8369ea75f52c7f572cb1cac384bdf
Alberto Lazari <alberto.lazari@gmail.com>
2025-02-27 11:34:11 +0100

    Height point based on y0

--- Assets/Scripts/BouncingBall.cs
+++ Assets/Scripts/BouncingBall.cs
@@ -3,29 +3,32 @@ using UnityEngine;
 public abstract class BouncingBall : MonoBehaviour
 {
     [SerializeField] protected Rigidbody2D m_Rigidbody;
+    [SerializeField] protected Collider2D m_Collider;
     [SerializeField] protected float m_HorizontalVelocity = 0.5f;
 
     protected abstract float TargetHeight { get; }
 
 
     // Vertical velocity required to reach the specific target height point
-    protected virtual float BounceVelocity()
+    protected virtual float BounceVelocity(float y0)
     {
         // Gravity might be scaled
         float gravity = Mathf.Abs(Physics2D.gravity.y * m_Rigidbody.gravityScale);
+
         // Y coordinates might be scaled as well
-        float height = TargetHeight / transform.localScale.y;
-        return Mathf.Sqrt(2 * gravity * height);
+        float dy = (TargetHeight - y0) / transform.localScale.y;
+
+        return Mathf.Sqrt(2 * gravity * dy);
     }
 
     protected virtual void Start()
     {
-        if (m_Rigidbody == null)
-        {
-            m_Rigidbody = GetComponent<Rigidbody2D>();
-        }
+        if (m_Rigidbody == null) m_Rigidbody = GetComponent<Rigidbody2D>();
+        if (m_Collider == null) m_Collider = GetComponent<Collider2D>();
+
         m_Rigidbody.constraints = RigidbodyConstraints2D.FreezeRotation;
         m_Rigidbody.linearVelocityX = m_HorizontalVelocity;
+
         // Assign fixed sorting order
         GetComponent<SpriteRenderer>().sortingOrder = Mathf.FloorToInt(transform.position.x * 100);
     }
@@ -41,7 +44,7 @@ public abstract class BouncingBall : MonoBehaviour
             }
             else if (point.y < 0f)
             {
-                OnFloorCollision();
+                OnFloorCollision(contact.point.y);
             }
             else
             {
@@ -57,10 +60,10 @@ public abstract class BouncingBall : MonoBehaviour
         m_Rigidbody.linearVelocityX = m_HorizontalVelocity;
     }
 
-    protected virtual void OnFloorCollision()
+    protected virtual void OnFloorCollision(float y0)
     {
         // Reset vertical velocity with static one
-        m_Rigidbody.linearVelocity = new Vector2(m_HorizontalVelocity, BounceVelocity());
+        m_Rigidbody.linearVelocity = new Vector2(m_HorizontalVelocity, BounceVelocity(y0));
     }
 
     protected virtual void OnCeilingCollision()
--- Assets/Scripts/SmallBall.cs
+++ Assets/Scripts/SmallBall.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class SmallBall : BouncingBall
 {
-    [SerializeField] private float m_TargetHeight = 0.92f;
+    [SerializeField] private float m_TargetHeight = 0.95f;
 
     protected override float TargetHeight => m_TargetHeight;
 }
