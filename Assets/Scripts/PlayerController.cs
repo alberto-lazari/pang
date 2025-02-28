a4469fad643a685de99f20e0e1b8bc304c51d936
Alberto Lazari <alberto.lazari@gmail.com>
2025-02-28 14:14:31 +0100

    Player controller rework

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,122 +2,32 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public enum MoveState
-    {
-        Idle   = 0,
-        Moving = 1,
-    }
+    [SerializeField] private float m_Speed = 1.5f;
+    [SerializeField] private Rigidbody2D m_Rigidbody;
+    [SerializeField] private Animator m_Animator;
+    [SerializeField] private SpriteRenderer m_SpriteRenderer;
 
-    public enum Direction
-    {
-        Left  = 0,
-        Right = 1,
-    }
-
-    [SerializeField] private float m_Speed = 1.3f;
-    [SerializeField] private float m_CollisionDistance = 0.12f;
-    [SerializeField] private Object m_Stage;
-    [SerializeField] private LayerMask m_WallLayer;
-
-    private Animator m_Animator;
-    private SpriteRenderer m_SpriteRenderer;
-
-    private static readonly int MoveStateHash = Animator.StringToHash("MoveState");
-    private static readonly int FacingDirectionHash = Animator.StringToHash("FacingDirection");
-
-    private MoveState m_MoveState = MoveState.Idle;
-    private Direction m_FacingDirection = Direction.Right;
-
-
-    public Vector2 GetDirectionVector()
-    {
-        return m_FacingDirection == Direction.Left ? Vector2.left : Vector2.right;
-    }
+    private static readonly int HorizontalInputHash = Animator.StringToHash("HorizontalInput");
 
     private void Start()
     {
-        if (m_Animator == null)
-        {
-            m_Animator = GetComponent<Animator>();
-        }
-        if (m_SpriteRenderer == null)
-        {
-            m_SpriteRenderer = GetComponent<SpriteRenderer>();
-        }
+        if (m_Rigidbody == null) m_Rigidbody = GetComponent<Rigidbody2D>();
+        if (m_Animator == null) m_Animator = GetComponent<Animator>();
+        if (m_SpriteRenderer == null) m_SpriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     private void Update()
     {
-        HandleInput();
-        UpdateAnimatorState();
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
 
-        // Flip sprite when facing left
-        m_SpriteRenderer.flipX = m_FacingDirection == Direction.Left;
+        // Update player speed
+        m_Rigidbody.linearVelocityX = horizontalInput * m_Speed;
 
-        if (CanMove())
-        {
-            Move();
-        }
-    }
+        // Update animator controller variable
+        m_Animator.SetFloat(HorizontalInputHash, horizontalInput);
 
-    private bool Facing(Direction i_Direction)
-    {
-        return m_FacingDirection == i_Direction;
-    }
-
-    private bool IsBlocked(Direction i_Direction)
-    {
-        // Do not cast rays from player's feet, otherwise they will catch the floor
-        Vector2 rayOrigin = (Vector2)transform.position + Vector2.up * 0.1f;
-        return Physics2D.Raycast(rayOrigin, GetDirectionVector(), m_CollisionDistance, m_WallLayer);
-    }
-
-    private void HandleInput()
-    {
-        bool bLeft = Input.GetKey(KeyCode.LeftArrow);
-        bool bRight = Input.GetKey(KeyCode.RightArrow);
-        bool bLeftDown = Input.GetKeyDown(KeyCode.LeftArrow);
-        bool bRightDown = Input.GetKeyDown(KeyCode.RightArrow);
-
-        switch (m_MoveState)
-        {
-        case MoveState.Idle:
-            if (bLeft || bRight)
-            {
-                m_MoveState = MoveState.Moving;
-                m_FacingDirection = bRight ? Direction.Right : Direction.Left;
-            }
-            break;
-        case MoveState.Moving:
-            // Change direction
-            if (bLeftDown && Facing(Direction.Right) || bRightDown && Facing(Direction.Left))
-            {
-                m_FacingDirection = bRightDown ? Direction.Right : Direction.Left;
-            }
-            // Stop
-            if (!bLeft && Facing(Direction.Left) || !bRight && Facing(Direction.Right))
-            {
-                m_MoveState = MoveState.Idle;
-            }
-            break;
-        }
-    }
-
-    private void UpdateAnimatorState()
-    {
-        m_Animator.SetInteger(MoveStateHash, (int)m_MoveState);
-        m_Animator.SetInteger(FacingDirectionHash, (int)m_FacingDirection);
-    }
-
-    private bool CanMove()
-    {
-        bool bMoving = m_MoveState == MoveState.Moving;
-        bool bNotBlocked = !IsBlocked(m_FacingDirection);
-        return bMoving && bNotBlocked;
-    }
-
-    private void Move()
-    {
-        transform.position += (Vector3)GetDirectionVector() * m_Speed * Time.deltaTime;
+        // Make sprite face the right direction
+        if (horizontalInput < 0.01f) m_SpriteRenderer.flipX = true;
+        else if (horizontalInput > 0.01f) m_SpriteRenderer.flipX = false;
     }
 }
