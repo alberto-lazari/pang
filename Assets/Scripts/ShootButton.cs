2a41def582e036959acad0d6efda13261ed51bff
Alberto Lazari <alberto.lazari@gmail.com>
2025-05-04 18:00:21 +0200

    UI prefab

--- Assets/Scripts/ShootButton.cs
+++ Assets/Scripts/ShootButton.cs
@@ -1,17 +1,21 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ShootButton : MonoBehaviour
 {
+    private Button m_Button;
     private SpriteRenderer m_SpriteRenderer;
 
     private void Awake()
     {
+        m_Button = GetComponent<Button>();
         m_SpriteRenderer = GetComponent<SpriteRenderer>();
         Weapon.OnWeaponPicked += UpdateButtonIcon;
     }
 
     private void UpdateButtonIcon(Weapon i_Weapon)
     {
+        m_Button.interactable = true;
         m_SpriteRenderer.sprite = i_Weapon.icon;
     }
 }
