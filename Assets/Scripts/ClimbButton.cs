a0002a736ca4da9a1d9553c059378115070ec732
Alberto Lazari <alberto.lazari@gmail.com>
2025-05-04 22:50:08 +0200

    Assets

--- Assets/Scripts/ClimbButton.cs
+++ Assets/Scripts/ClimbButton.cs
@@ -4,11 +4,13 @@ using UnityEngine.UI;
 public class ClimbButton : MonoBehaviour
 {
     private Button m_Button;
+    private Image m_Image;
     private SpriteRenderer m_SpriteRenderer;
 
     private void Awake()
     {
         m_Button = GetComponent<Button>();
+        m_Image = m_Button.GetComponent<Image>();
         m_SpriteRenderer = GetComponent<SpriteRenderer>();
         PlayerController.OnNearLadder += SetButtonActive;
     }
@@ -18,6 +20,7 @@ public class ClimbButton : MonoBehaviour
         if (m_SpriteRenderer == null) return;
 
         m_Button.interactable = i_IsActive;
+        m_Image.raycastTarget = i_IsActive;
         m_SpriteRenderer.enabled = i_IsActive;
     }
 }
